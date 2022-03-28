5d5d4270b0a7cf9b40cb2d585dcb55c8878cea0b
strcurseten <fernandez.kirstenpearl@auf.edu.ph>
2022-03-28 14:51:24 +0800

    Fixed error and formatted all-chapters

--- resources/views/all-chapters.blade.php
+++ resources/views/all-chapters.blade.php
@@ -3,15 +3,29 @@
 
     </head>
     <title>
-
+        All Six Of Crows Part One Chapter
     </title>
+    <style>
+        .content{
+            padding: 10px 10px;
+            display: flex;
+            justify-content: center;
+        }
+        pre{
+            margin: 10px;
+            padding: 50px 90px;
+            border: 1px solid grey;
+        }
+    </style>
     <body>
-        <h1>{{ $book_name }}</h1>
-        <h2>{{ $part_name }}</h2>
+        <h1 style="text-align:center">{{ $book_name }}</h1>
+        <h2 style="text-align:center">{{ $part_name }}</h2>
         @for($i = 1; $i <= $chapter_number; $i++)
-            <h3>Chapter {{ $i }}</h3>
             <hr>
-            <pre>@include("chapter{ $i }")</pre>
+            <h2 style="text-align:center">Chapter {{ $i }}</h3>
+            <div class="content">
+                <pre><span style="font-size: 20px; font-family:Times New Roman;">@include("chapter" . $i)<span></pre>
+            </div>
         @endfor 
     </body>
 </html>
